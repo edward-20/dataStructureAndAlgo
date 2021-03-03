def3023de3c8233f0610c113b33af2138ce1bac7
Edward Lim <ed.lim199@gmail.com>
2021-03-03 17:43:42 +1100

    Finished implementing bubble sort

--- sorting/simple/bubbleSort.c
+++ sorting/simple/bubbleSort.c
@@ -3,6 +3,34 @@
  * them, thereby "bubbling" the higher results to where they belong in the
  * ordered array.
  */
+#include <stdbool.h>
+
 void bubbleSort(arr[], int l, int r)
 {
+	// Iterator
+	int i;
+	// Each iteration equates to a "pass", that ultimately "bubbles" the
+	// largest key to the end of the array
+	for (i = l; i <= r; i++) {
+
+		// Check to see in the pass if the array is already sorted
+		bool sorted = true;
+		// Iterate through the supposedly unsorted subarray:
+		// arr[l .. r - i]
+		int j;
+		for (j = l; j <= r - i; j++) {
+			if (arr[j] < arr[j + 1]) {
+				// swap the keys
+				int buffer = arr[j + 1];
+				arr[j + 1] = arr[j];
+				arr[j] = buffer;
+				sorted = false;
+			}
+		}
+		
+		// If the array is sorted we needn't continue the function
+		if (sorted)
+			return;
+	}
+	return;
 }
